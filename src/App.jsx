dde33d4b877295c15cb79a7d9b2e942a6657d890
farukalways <alishopon367@gmail.com>
2025-08-25 23:20:29 +0600

    fake login and logout setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import LoginPage from "./page/LoginPage";
 import ProfilePage from "./page/ProfilePage";
 import RegistrationPage from "./page/RegistrationPage";
 import NotFoundPage from "./page/NotFoundPage";
+import PrivateRoutes from "./routes/PrivateRoutes";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<HomePage />} path="/" exact />
+        <Route element={<PrivateRoutes />}>
+          <Route element={<HomePage />} path="/" exact />
+          <Route element={<ProfilePage />} path="/me" />
+        </Route>
         <Route element={<LoginPage />} path="/login" />
-        <Route element={<ProfilePage />} path="/me" />
         <Route element={<RegistrationPage />} path="/register" />
         <Route element={<NotFoundPage />} path="*" />
       </Routes>
