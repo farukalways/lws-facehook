0cbaeb1a1d9debe338868b1faa816dcb9d44e4ed
farukalways <alishopon367@gmail.com>
2025-08-29 08:26:20 +0600

    profile page update

--- src/page/ProfilePage.jsx
+++ src/page/ProfilePage.jsx
@@ -1,5 +1,47 @@
+import { useAuth } from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
+import { useEffect, useState } from "react";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  console.log(error);
+
+  const { api } = useAxios();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchProfile = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user?.id}`
+        );
+        setUser(response?.data?.user);
+        setPosts(response?.data?.posts);
+      } catch (error) {
+        console.error(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, [api, auth?.user?.id]);
+
+  if (loading) {
+    return <div> Fetching your Profile data...</div>;
+  }
+
+  return (
+    <div>
+      Welcome, {user?.firstName} {user?.lastName}
+      <p>You have {posts.length} posts.</p>
+    </div>
+  );
 };
 
 export default ProfilePage;
