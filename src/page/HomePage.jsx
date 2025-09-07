4e5655b0280a3243cc273c18265c706610f88021
farukalways <alishopon367@gmail.com>
2025-09-07 21:51:02 +0600

    like and comment functionality add

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,8 +1,52 @@
+import { useEffect, useReducer } from "react";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
+
+import { initialState, postReducer } from "../reducers/postReducer";
+
+import useAxios from "../hooks/useAxios";
+
 const HomePage = () => {
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+
+    const fetchPost = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+        if (response.status === 200) {
+          dispatch({
+            type: actions.post.DATA_FETCHED,
+            data: response.data,
+          });
+        }
+      } catch (error) {
+        console.error(error);
+        dispatch({
+          type: actions.post.DATA_FETCH_ERROR,
+          error: error.message,
+        });
+      }
+    };
+
+    fetchPost();
+  }, [api]);
+
+  if (state?.loading) {
+    return <div> We are working...</div>;
+  }
+
+  if (state?.error) {
+    return <div> Error in fatching posts {state?.error?.message}</div>;
+  }
+
   return (
     <div>
-      <h2>eee</h2>
-      <h2>eee</h2>
+      <PostList posts={state?.posts} />
     </div>
   );
 };
